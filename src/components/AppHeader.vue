06b88db5b17623515131a80a1bcfa946927f5547
Al-Giovinetti <giovinettialessio@gmail.com>
2023-08-31 16:13:39 +0200

    Collegate rotte dall' header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,9 +1,41 @@
 <script>
+    export default{
+        name:'AppHeader',
+
+        data(){
+            return{
+
+                navElements:[
+            {
+                routeName:'home',
+                name:'Home'
+            },
+            {
+                routeName:'projects',
+                name:'Projects'
+            },
+        ]
+
+            }
+        }
+    }
 
 </script>
 
 <template>
-<h1>title</h1>
+<header>
+    <h1>Titolastrogrande</h1>
+    <nav>
+        <ul>
+            <li v-for="element in navElements">
+                <router-link :to=" { name: element.routeName}">
+                    {{ element.name }}
+                </router-link>
+            </li>
+        </ul>
+
+    </nav>
+</header>
 </template>
 
 <style lang="scss" scoped >
